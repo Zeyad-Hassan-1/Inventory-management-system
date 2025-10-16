aaf9fb766ff9964aed16ac839d9b64aa534d4c47
zeyad-hassan-1 <studying.zezo@gmail.com>
2025-10-17 00:26:31 +0300

    Setub initial database structure

---
+++ Inventory_management_system/src/main/java/com/company/inventory_management_system/database/Database.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.company.inventory_management_system.database;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author zeyad
+ */
+public abstract class Database {
+    private String fileName;
+    private ArrayList<Record> records;
+}
