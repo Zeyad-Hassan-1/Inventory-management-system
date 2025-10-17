0d1742ec65c341bfb76c499119b2807e047f739d
zeyad-hassan-1 <studying.zezo@gmail.com>
2025-10-17 03:05:22 +0300

    Extend Database.java and override CreateRecordFrom method in each record type

--- Inventory_management_system/src/main/java/com/company/inventory_management_system/database/EmployeeUserDatabase.java
+++ Inventory_management_system/src/main/java/com/company/inventory_management_system/database/EmployeeUserDatabase.java
@@ -4,10 +4,32 @@
  */
 package com.company.inventory_management_system.database;
 
+import com.company.inventory_management_system.admin_role.EmployeeUser;
+
 /**
  *
  * @author zeyad
  */
 public class EmployeeUserDatabase extends Database{
+
+    public EmployeeUserDatabase(String fileName) {
+        super(fileName);
+    }
+
+    @Override
+    public Record createRecordFrom(String line) {
+        if(line == null){
+            System.out.println("please input the data separated by ,");
+            return null;
+        }
+        String[] fields = line.replace(" ","").split(",");
+        if(fields.length != 5){
+            System.out.println("Invalid line input");
+            return  null;
+        }
+        return  new EmployeeUser(fields[0],fields[1],fields[2],fields[3],fields[4]);
+    }
+    
+    
     
 }
--- Inventory_management_system/src/main/java/com/company/inventory_management_system/database/ProductDatabase.java
+++ Inventory_management_system/src/main/java/com/company/inventory_management_system/database/ProductDatabase.java
@@ -4,10 +4,30 @@
  */
 package com.company.inventory_management_system.database;
 
+import com.company.inventory_management_system.admin_role.Product;
+
 /**
  *
  * @author zeyad
  */
-public class ProductDatabase extends Database{
-    
+public class ProductDatabase extends Database {
+
+    public ProductDatabase(String fileName) {
+        super(fileName);
+    }
+
+    @Override
+    public Record createRecordFrom(String line) {
+        if (line == null) {
+            System.out.println("please input the data separated by ,");
+            return null;
+        }
+        String[] fields = line.replace(" ", "").split(",");
+        if (fields.length != 6) {
+            System.out.println("Invalid line input");
+            return null;
+        }
+        return new Product(fields[0], fields[1], fields[2], fields[3], Integer.parseInt(fields[4]), Float.parseFloat(fields[5]));
+    }
+
 }
