bb6f5d4bb785cf8077cf52d9d594233a900d01a9
menna <mennashi31@gmail.com>
2025-10-18 14:44:23 +0300

    add customerProuduct class

--- Inventory_management_system/src/main/java/com/company/inventory_management_system/CustomerProduct.java
+++ Inventory_management_system/src/main/java/com/company/inventory_management_system/CustomerProduct.java
@@ -4,34 +4,55 @@
  */
 package com.company.inventory_management_system;
 
-import com.company.inventory_management_system.database.Record;
 import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import com.company.inventory_management_system.database.Record;
 
 /**
  *
  * @author zeyad
  */
-public class CustomerProduct implements Record {
-    private String customerSSN, productID;
+public class CustomerProduct implements Record{
+    private String customerSSN;
+    private String productID;
     private LocalDate purchaseDate;
     private boolean paid;
+    private static final DateTimeFormatter FORMATTER = DateTimeFormatter.ofPattern("dd-MM-yyyy");
 
     public CustomerProduct(String customerSSN, String productID, LocalDate purchaseDate) {
         this.customerSSN = customerSSN;
         this.productID = productID;
         this.purchaseDate = purchaseDate;
+        this.paid = false;
     }
-    
-    
+
+    public String getCustomerSSN() {
+        return customerSSN;
+    }
+
+    public String getProductID() {
+        return productID;
+    }
+
+    public LocalDate getPurchaseDate(){
+        return purchaseDate;
+    }
+
+    public boolean isPaid() {
+        return paid;
+    }
+
+    public void setPaid(boolean paid) {
+        this.paid = paid;
+    }
 
     @Override
     public String lineRepresentation() {
-        return null;
+        return customerSSN + "," + productID + "," + purchaseDate.format(FORMATTER) + "," + paid;
     }
 
     @Override
     public String getSearchKey() {
-        return null;
-    }
-
+        return customerSSN + "," + productID + "," + purchaseDate.format(FORMATTER);
+    }  
 }
